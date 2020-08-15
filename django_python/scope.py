9b79c05b5ffa8865710454f9ec9460ca8a55984e
maliksh7 <p176137@nu.edu.pk>
2020-08-15 22:21:26 +0500

    scope file added

---
+++ django_python/scope.py
@@ -0,0 +1,47 @@
+# Scope in Python
+# |-> Python Scope follows the LEGB Rule:
+#       * Local
+#       * Enclosing Function Locals
+#       * Global
+#       * Built-in
+
+x = 25
+
+def my_fun():
+    x = 50
+    return x
+
+# print(x)            #expected output - 25
+# print(my_fun())     #expected output - 50
+
+my_fun()            #expected out ? - take a while and think..
+print(x)            #expected output - 25 -> this is due to Global Scope
+
+# Local
+
+lambda x: x ** 2
+
+        #  another Example
+x = 50
+def fun(x):
+    print("x is: ",x)                   #output Global x = 50, because x isnot declared locally yet
+    x = 1000                            #local x
+    print("local x changed to: ",x)     #output local x = 1000
+
+fun(x)
+
+# Enclosing Function Locals
+
+name = "This is global name"
+
+def greet():
+    name = "Sammy"
+
+    def hello():
+        print("Hello",name)
+    hello()
+
+greet()         #EFL -> Hello Sammy
+print(name)     #Global - This is global name
+
+# Built-in
